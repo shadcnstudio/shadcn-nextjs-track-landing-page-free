9f0fd39bc171af6c461f2701f88d55900753ac48
CodeByOmm <patelomc33@gmail.com>
2026-01-12 19:37:22 +0530

    refactor(landing-page): updated v1_1_0.tsx as per suggestion

--- src/components/blocks/changelog-section/v1_1_0 .tsx
+++ src/components/blocks/changelog-section/v1_1_0 .tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import TimelineItem from '@/components/timeline/timeline-items'
 import Accordion from '@/components/badge-accordion/badge-accordion'
 import { accordionDataV1_1_0 } from '@/assets/data/changelog-data'
@@ -26,7 +28,9 @@ function v1_1_0() {
             consistent visuals across all environments. Tokens for colors, spacing, typography, and more are now fully
             type-safe and easily customizable, ensuring that your design system can grow alongside your projects.{' '}
           </p>
-          <Button>Read More</Button>
+          <Button asChild>
+            <Link href='#'>Read More</Link>
+          </Button>
           <Accordion data={accordionDataV1_1_0} />
         </div>
       </TimelineItem>
